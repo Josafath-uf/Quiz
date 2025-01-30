5c4dcea7eeeabec19179d7bf35468bf34e5ae1b9
Josafath-uf <minilu0789@gmail.com>
2025-01-29 20:41:42 -0600

    quiz
    no esta terminado

---
+++ quiz/src/quiz/Quiz.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author minil
+ */
+public class Quiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String salario="";
+    salario=JOptionPane.showInputDialog("Digite su salario");
+    System.out.println("El salario es de:"+salario);
+    
+    //Monto SEM
+    
+    
+    
+    }
+    
+}
